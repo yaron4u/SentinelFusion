9c9dfbaa3d4998c6fac14e9749f43b0e15f13dfd
yaron4u <yaronbereza@gmail.com>
2023-09-26 21:16:06 +0300

    beta v0.1 (Protocol Identification)

--- threat_detection.py
+++ threat_detection.py
@@ -2,6 +2,16 @@ from scapy.all import sniff
 
 times_found_dict = {}
 
+PROTOCOL_LOOKUP = {
+    1: "ICMP",
+    2: "IGMP",
+    6: "TCP",
+    17: "UDP",
+    41: "IPv6",
+    89: "OSPF",
+    132: "SCTP",
+}
+
 
 def increment_or_add_to_dict(protocol):
     if protocol in times_found_dict.keys():
@@ -12,7 +22,8 @@ def increment_or_add_to_dict(protocol):
 
 def packet_callback(packet):
     if packet.haslayer("IP"):
-        packet_protocol = packet["IP"].proto
+        protocol_num = packet["IP"].proto
+        packet_protocol = PROTOCOL_LOOKUP.get(protocol_num, str(protocol_num))
         increment_or_add_to_dict(packet_protocol)
         # this print shows the full content of the packet, leave it be
         # print(packet.show())
